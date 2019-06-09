12215a25e4110ddf216b28a8d4992b8d5933f0ae
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-06-09 10:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"]
+  "He hated the sound of his own voic"]
   
 let newarr = []
 
