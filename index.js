94f8b540a4e86af8dc7e3ec32498c9309a3cccc0
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-06-09 10:56:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // add solution here
 
+let musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr']
+let instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
+let empty = []
+
+function theBeatlesPlay(musicians, instruments){
+	for(let a = 0; a < musicians.length; a++){
+    empty.push(`${musicians[a]} plays ${instruments[a]}`)
+  }
+  return empty
+}
 
 
 
