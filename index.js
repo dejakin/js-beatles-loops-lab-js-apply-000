77f43574623b5bee7bac55d967f7bb01dacea3c5
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-06-09 10:37:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ function theBeatlesPlay(musicians, instruments){
   return empty
 }
 
-let facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice",
-  "foo",
-  "bar"]
+let facts = [,"foo", "bar"]
   
 let newarr = []
 
