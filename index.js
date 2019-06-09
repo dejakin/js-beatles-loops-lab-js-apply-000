6d7c27097fc06fadeb6abbe0066fb0753bf6fd9f
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-06-09 10:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 // add solution here
 
-let musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr']
-let instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
-let empty = []
-
-function theBeatlesPlay(musicians, instruments){
-	for(let a = 0; a < musicians.length; a++){
-    empty.push(`${musicians[a]} plays ${instruments[a]}`)
-  }
-  return empty
-}
-
 let facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
