497e7fc941ce4c84e4bcb63b5973d3656be4b4e3
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-06-09 09:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let instruments = ['Guitar', 'Piano']
 let empty = []
 
 function theBeatlesPlay(musicians, instruments){
-  for(let a = 0; a < musicians.length; a++){
-    musicians[a]
+	for(let a = 0; a < musicians.length; a++){
+    empty.push(`${musicians[a]} plays ${instruments[a]}`)
   }
 }
+
